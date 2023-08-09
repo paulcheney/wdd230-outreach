db51bcd0282f71ca340a2894c1bc450fa7881180
Paul Cheney <spartandesign.biz@gmail.com>
2023-08-09 08:29:30 -0600

    outreach

---
+++ starts.js
@@ -0,0 +1,7 @@
+//const blockStart = new Date('1/22/2023').getTime();
+export const blockStart = new Date('09/11/2023').getTime(); // random day
+export const week2start = new Date('09/18/2023').getTime(); // first Monday after block starts
+export const blockEnd = new Date('10/24/2023').getTime();
+
+export const monthList= ["January","February","March","April","May","June","July",
+"August","September","October","November","December"];
