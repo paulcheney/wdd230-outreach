bb23d6c4cdc3930d265f4f4c5e0dc0d0e84105eb
Paul Cheney <spartandesign.biz@gmail.com>
2023-10-20 11:47:34 -0600

    update semester start times

--- starts.js
+++ starts.js
@@ -1,7 +1,7 @@
 //const blockStart = new Date('1/22/2023').getTime();
-export const blockStart = new Date('09/11/2023').getTime(); // random day
-export const week2start = new Date('09/18/2023').getTime(); // first Monday after block starts
-export const blockEnd = new Date('10/24/2023').getTime();
+export const blockStart = new Date('10/25/2023').getTime(); // random day
+export const week2start = new Date('11/06/2023').getTime(); // first Monday after block starts
+export const blockEnd = new Date('12/13/2023').getTime();
 
 export const monthList= ["January","February","March","April","May","June","July",
 "August","September","October","November","December"];
