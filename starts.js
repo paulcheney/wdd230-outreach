4b17c2d2b68ae3ce900d1d0b8fb4dd8bc8999cdd
paulcheney <spartandesign.biz@gmail.com>
2024-12-26 14:48:24 -0600

    new dates

--- starts.js
+++ starts.js
@@ -1,7 +1,7 @@
 //const blockStart = new Date('1/22/2023').getTime();
-export const blockStart = new Date('10/30/2024').getTime(); // random day
-export const week2start = new Date('11/11/2024').getTime(); // first Monday after block starts
-export const blockEnd = new Date('12/18/2024').getTime();
+export const blockStart = new Date('01/06/2025').getTime(); // random day
+export const week2start = new Date('01/13/2025').getTime(); // first Monday after block starts
+export const blockEnd = new Date('02/22/2025').getTime();
 
 export const monthList= ["January","February","March","April","May","June","July",
 "August","September","October","November","December"];
